b799548eefbea25d47db965ea0f061eef307f7aa
royviggo <royviggo@gmail.com>
2018-06-07 22:52:37 +0200

    Auto property for DbContext

--- DotNetCore.Data/Database/UnitOfWork.cs
+++ DotNetCore.Data/Database/UnitOfWork.cs
@@ -6,13 +6,12 @@ namespace DotNetCore.Data.Database
 {
     public class UnitOfWork : IUnitOfWork, IDisposable
     {
-        private readonly DotNetCoreContext _context;
-
         public UnitOfWork(IDbFactory dbFactory)
         {
-            _context = dbFactory.GetDbContext();
+            DbContext = dbFactory.GetDbContext();
         }
-        public DotNetCoreContext DbContext => _context;
+
+        public DotNetCoreContext DbContext { get; }
 
         private IGenericRepository<Person> _personRepository;
         public IGenericRepository<Person> PersonRepository => _personRepository ?? (_personRepository = new GenericRepository<Person>(DbContext));
